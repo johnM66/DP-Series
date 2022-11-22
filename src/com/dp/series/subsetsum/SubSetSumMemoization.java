0d149c4f4010441db219823343ce0f9bae171059
johnM66 <mkmaity.dev@gmail.com>
2022-11-22 21:44:21 +0530

    subset sum all pattern

--- src/com/dp/series/subsetsum/SubSetSumMemoization.java
+++ src/com/dp/series/subsetsum/SubSetSumMemoization.java
@@ -6,17 +6,26 @@ public class SubSetSumMemoization {
         int set[] = {3, 34, 4, 12, 5, 2};
         int sum = 9;
         int n = set.length;
-         subSetSumMemo(set, sum, n);
-    }
-
-    private static void subSetSumMemo(int[] set, int sum, int n) {
-        boolean[][] dp = new boolean[n+1][sum+1];
-        for (int i = 0; i <= n+1; i++)
-            for (int j = 0; j <= sum+1; j++) {
-                dp[i][j] = false;
+        boolean dp[][] = new boolean[n + 1][sum + 1];
+        for (int i = 0; i < n + 1; i++)
+            for (int j = 0; j < sum + 1; j++) {
+                dp[i][j] = Boolean.FALSE;
             }
+        System.out.println(subSetSumMemo(set, sum, n,dp));
     }
 
+    private static boolean subSetSumMemo(int[] set, int sum, int n, boolean[][] dp) {
+
+        if (n == 0)
+            return false;
+        if (sum == 0)
+            return true;
+        if (set[n - 1] <= sum) {
+            return dp[n][sum] = subSetSumMemo(set, sum - set[n - 1], n - 1, dp) || subSetSumMemo(set, sum, n - 1, dp);
+        } else {
+            return dp[n][sum] = subSetSumMemo(set, sum, n - 1, dp);
+        }
+    }
 
 
 }
